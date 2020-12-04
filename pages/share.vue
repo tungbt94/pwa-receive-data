6de76b271e96457b0b3843d757de1c5d9552eb4b
buithanhtung <buithanhtung@sutunam.com>
2020-12-04 13:57:19 +0700

    feat: add screenshot function

--- pages/share.vue
+++ pages/share.vue
@@ -3,24 +3,53 @@
     <p>title: {{ title }}</p>
     <p>text: {{ text }}</p>
     <p>url: {{ url }}</p>
+    <img v-if="src" :src="src" :alt="url" />
   </div>
 </template>
 
 <script>
+import { bufferToImageUrl } from '@/utils/image'
+
 export default {
   data() {
     return {
       title: '',
       text: '',
       url: '',
+      src: '',
     }
   },
 
   mounted() {
-    const parsedUrl = new URL(window.location)
-    this.title = parsedUrl.searchParams.get('title')
-    this.text = parsedUrl.searchParams.get('text')
-    this.url = decodeURIComponent(parsedUrl.searchParams.get('url')) + '.html'
+    this.syncDataFromParam()
+    this.getScreenshot()
+  },
+
+  methods: {
+    syncDataFromParam() {
+      const parsedUrl = new URL(window.location)
+      this.title = parsedUrl.searchParams.get('title')
+      this.text = parsedUrl.searchParams.get('text')
+      this.url = decodeURIComponent(parsedUrl.searchParams.get('url')) + '.html'
+    },
+    getScreenshot() {
+      const screenshotFunctionUrl =
+        'https://pwa-receive-data.netlify.app/.netlify/functions/screenshot'
+      const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json; charset=utf-8' },
+        body: JSON.stringify({ pageToScreenshot: this.url }),
+      }
+      fetch(screenshotFunctionUrl, options)
+        .then((res) => res.json())
+        .then((res) => {
+          if (!res.buffer) console.log('Capture screenshot error')
+          this.src = bufferToImageUrl(res.buffer.data)
+        })
+        .catch((err) => {
+          console.error('error:', err)
+        })
+    },
   },
 }
 </script>
---
+++ utils/image.js
@@ -0,0 +1,9 @@
+function bufferToImageUrl(buffer) {
+  const arrayBufferView = new Uint8Array(buffer)
+  const blob = new Blob([arrayBufferView], { type: 'image/jpeg' })
+  const urlCreator = window.URL || window.webkitURL
+  const imageUrl = urlCreator.createObjectURL(blob)
+  return imageUrl
+}
+
+export default { bufferToImageUrl }
